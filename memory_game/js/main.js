1b066f791e5dc5b2d018072748cdd5c07ed92da2
ASSROUGUI <ASSROUGUI@hotmail.com>
2019-05-26 04:41:47 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,26 @@
- // console.log("Up and running!");
 
- // var cardOne = "queen";
- // var cardTwo = "king";
- // var cardThree = "king";
- // var cardFour = "queen";
- // console.log("User flipped " + cardOne);
- // console.log("User flipped " + cardTwo);
 
  var cards = [ "queen", "queen", "king", "king"];
  var cardsInPlay = [];
- var cardOne =  "queen";
- cardsInPlay.push(cardOne);
- console.log("User flipped " + cardOne);
- var cardTwo ;
- cardTwo = cards[3];
- cardsInPlay.push(cardTwo);
- console.log("User flipped " + cardTwo);
- if (cardsInPlay.length === 2){
- 	// console.log(true);}
 
- if (cardsInPlay[0] === cardsInPlay[1]) {
- 	alert("You found a match!");}
- 	else {alert("Sorry, try again ");}}
- else {console.log(false);}
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");}
+}
 
+ function flipCard(cardId){
+console.log("User flipped " + cardId);
+	// for(var i=0;i<cards.length;i++){
+cardsInPlay.push(cardId);}
+
+
+
+flipCard(cards[0]);
+flipCard(cards[2]);
+
+checkForMatch();
 
 
 
